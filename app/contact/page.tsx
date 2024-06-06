a90aa8d401293ab45864407da6395e9d559809f3
Jarviss77 <gupta.shubham2404@gmail.com>
2024-06-06 14:06:34 +0530

    updated home and contacts page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,27 +1,45 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import {Github, Mail, Instagram, Linkedin, Twitter} from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
 	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
-	},
-	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:gupta.shubham72404@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "Shubham Gupta"
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/Jarviss77",
 		label: "Github",
-		handle: "chronark",
+		handle: "Jarviss77",
+	},
+	{
+		icon: <Linkedin size={20} />,
+		href: "https://www.linkedin.com/in/shubham-gupta-5043a4237/",
+		label: "Linkedin",
+		handle: "Shubham Gupta",
+	},
+	{
+		icon: <Instagram size={20} />,
+		href: "https://www.instagram.com/shubhamm.guptaa/",
+		label: "Instagram",
+		handle: "shubhamm.guptaa",
+	},
+	{
+		icon: <Twitter size={20} />,
+		href: "https://x.com/Shubhamm_Guptaa",
+		label: "Twitter",
+		handle: "@Shubhamm_Guptaa",
+	},
+	{
+		icon: <Github size={20} />,
+		href: "https://github.com/Jarviss77",
+		label: "Github",
+		handle: "Jarviss77",
 	},
 ];
 
@@ -30,7 +48,7 @@ export default function Example() {
 		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
 			<Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
+				<div className="grid w-full grid-cols-1 gap-8 mx-auto my-32 sm:grid-cols-3 lg:gap-16">
 					{socials.map((s) => (
 						<Card>
 							<Link
